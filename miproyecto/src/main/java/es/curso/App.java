cc128c2ec8a53addc2739f0b2bf89188ce15e946
Ubuntu <ubuntu@ip-172-31-1-165.eu-west-1.compute.internal>
2020-12-14 18:41:15 +0000

    pruebas

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -10,4 +10,21 @@ public class App
     {
         System.out.println( "Hola Jenkins!" );
     }
+    
+    public static int doblar( int numero )
+    {
+        return numero*2;
+    }
+    
+public static int mayor( int numero1, int numero2 )
+    {
+        /*if (numero1 > numero2) {
+            return numero1
+        }
+        return numero2;
+        */
+        return numero1 > numero2 ? numero1 : numero2 ;
+    }
+
+
 }
